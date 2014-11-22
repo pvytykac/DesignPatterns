ae643bf25049cb7a4dc2b1e14fed542b1e607343
pvytykac <pvytykac@gmail.com>
2014-11-22 19:33:41 +0100

    singleton

--- src/test/java/org/pvytykac/creational/singleton/SingletonTest.java
+++ src/test/java/org/pvytykac/creational/singleton/SingletonTest.java
@@ -2,13 +2,6 @@ package org.pvytykac.creational.singleton;
 
 import org.junit.Test;
 
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.CyclicBarrier;
-
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -32,84 +25,103 @@ public class SingletonTest {
         assertTrue(s4 == LazyThreadSafeSynchronizedSingleton.getInstance());
     }
 
-    @Test
-    public void multithreadedSingletonTest(){
-        final int threadCount = 100;
-        Map<Class,Long> map = new HashMap<>();
-        Class[] arr = new Class[]{
-                LazyThreadSafeSynchronizedSingleton.class,
-                LazyThreadSafeSyncBlockSingleton.class,
-                EagerThreadSafeSingleton.class,
-        };
-
-        CyclicBarrier gate = new CyclicBarrier(threadCount + 1);
-        List<Thread> liThread = new ArrayList<>();
-        for(Class clazz: arr){
-            try{
-                liThread.clear();
-                gate.reset();
-
-                // start all the threads
-               for(int i = 0; i < threadCount; i++){
-                   Thread t = new Thread(new MultiThreadedSingletonTestRunnable(clazz, gate));
-                   liThread.add(t);
-                   t.start();
-                }
-
-                // wait for all the threads to get to the gate
-                // get current time
-                // release the gate to allow threads to continue executing
-                // wait for all the threads to complete
-                gate.await();
-                long time = System.currentTimeMillis();
-                for(Thread t: liThread){
-                    t.join();
-                }
-
-                // get the time delta
-                // write the result to the map
-                time = (-time + System.currentTimeMillis());
-                System.out.println("class: "
-                        + clazz.getName()
-                        + ", duration: "
-                        + time
-                        + " ms");
-
-                map.put(clazz, time);
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-        }
-
-        // LazyLoadedSingleton with synchronized method should be slower then the other two
-        assertTrue(map.get(EagerThreadSafeSingleton.class) < map.get(LazyThreadSafeSynchronizedSingleton.class));
-        assertTrue(map.get(LazyThreadSafeSyncBlockSingleton.class) < map.get(LazyThreadSafeSynchronizedSingleton.class));
-    }
-
-    private class MultiThreadedSingletonTestRunnable implements Runnable {
-
-        private Class singletonClazz;
-        private CyclicBarrier gate;
-
-        private MultiThreadedSingletonTestRunnable(Class singletonClazz, CyclicBarrier gate) {
-            this.singletonClazz = singletonClazz;
-            this.gate = gate;
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public void run() {
-            try{
-                gate.await();
-                Method method = singletonClazz.getDeclaredMethod("getInstance");
-                method.setAccessible(true);
-                for(int i = 0; i < 100000; i++){
-                    method.invoke(null);
-                }
-            }catch(Exception ex){
-                ex.printStackTrace();
-            }
-        }
-    }
+//    @Test
+//    public void multithreadedSingletonTest(){
+//        final int threadCount = 100;
+//        Map<Class,Long> map = new HashMap<>();
+//        Class[] arr = new Class[]{
+//                LazyThreadSafeSynchronizedSingleton.class,
+//                LazyThreadSafeSyncBlockSingleton.class,
+//                EagerThreadSafeSingleton.class,
+//        };
+//
+//        CyclicBarrier gate = new CyclicBarrier(threadCount + 1);
+//        List<Thread> liThread = new ArrayList<>();
+//        List<MultiThreadedSingletonTestRunnable> liRunnable = new ArrayList<>();
+//        long[] ids = new long[threadCount];
+//        for(Class clazz: arr){
+//            try{
+//                liThread.clear();
+//                gate.reset();
+//
+//                // start all the threads
+//               for(int i = 0; i < threadCount; i++){
+//                   MultiThreadedSingletonTestRunnable r = new MultiThreadedSingletonTestRunnable(clazz, gate);
+//                   Thread t = new Thread(r);
+//                   liThread.add(t);
+//                   liRunnable.add(r);
+//                   t.start();
+//                   ids[i] = t.getId();
+//                }
+//
+//                // wait for the gate
+//                // wait for all the threads to complete
+//                gate.await();
+//                for(Thread t: liThread){
+//                    t.join();
+//                }
+//
+//                long time = 0;
+//                for(MultiThreadedSingletonTestRunnable r: liRunnable){
+//                    time += r.getCpuTime();
+//                }
+//
+//                // get the cpu time
+//                // write the result to the map
+//                System.out.println("class: "
+//                        + clazz.getName()
+//                        + ", duration: "
+//                        + time
+//                        + " ms");
+//
+//                map.put(clazz, time);
+//            }catch(Exception ex){
+//                ex.printStackTrace();
+//            }
+//        }
+//
+//        // LazyLoadedSingleton with synchronized method should be slower then the other two
+//        assertTrue(map.get(EagerThreadSafeSingleton.class) < map.get(LazyThreadSafeSynchronizedSingleton.class));
+//        assertTrue(map.get(LazyThreadSafeSyncBlockSingleton.class) < map.get(LazyThreadSafeSynchronizedSingleton.class));
+//    }
+//
+//    private class MultiThreadedSingletonTestRunnable implements Runnable {
+//
+//        private Class singletonClazz;
+//        private CyclicBarrier gate;
+//        private long cpuTime;
+//
+//        private MultiThreadedSingletonTestRunnable(Class singletonClazz, CyclicBarrier gate) {
+//            this.singletonClazz = singletonClazz;
+//            this.gate = gate;
+//        }
+//
+//        @Override
+//        @SuppressWarnings("unchecked")
+//        public void run() {
+//            try{
+//                gate.await();
+//                Method method = singletonClazz.getDeclaredMethod("getInstance");
+//                method.setAccessible(true);
+//                for(int i = 0; i < 100000; i++){
+//                    method.invoke(null);
+//                }
+//
+//                this.cpuTime = countCpuTime();
+//            }catch(Exception ex){
+//                ex.printStackTrace();
+//            }
+//        }
+//
+//        public long getCpuTime(){
+//            return cpuTime;
+//        }
+//
+//        public long countCpuTime() {
+//            ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+//            return bean.isCurrentThreadCpuTimeSupported( ) ?
+//                    (bean.getCurrentThreadCpuTime() - bean.getCurrentThreadUserTime()) : 0L;
+//        }
+//    }
 
 }
